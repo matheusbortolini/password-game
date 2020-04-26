147d0a2a0325d79f0e92f39d8ea0c6674ff17a10
unknown <matheusbortolini@hotmail.com>
2020-04-26 17:25:03 -0300

    get user guess

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	for i := 0; i < 10; i++ {
-		allowRepetition := true
-		fmt.Println(generateRandomPassword(allowRepetition))
-		time.Sleep(1)
+	allowRepetition := false
+	maxGuesses := 1
+	password := generateRandomPassword(allowRepetition)
+
+	fmt.Println(password)
+
+	var guess [4]int
+
+	for guessCount := 0; guessCount < maxGuesses; guessCount++ {
+		guess = getUserGuess(guessCount)
+		fmt.Println(guess)
 	}
 }
 
@@ -35,4 +46,39 @@ func generateRandomPassword(allowRepetition bool) [4]int {
 	}
 
 	return password
+}
+
+func getUserGuess(guessCount int) [4]int {
+	var guess [4]int
+	scanner := bufio.NewScanner(os.Stdin)
+
+OUTER:
+	for {
+		fmt.Printf("Tentativa %d: ", guessCount)
+
+		if scanner.Scan() {
+			line := scanner.Text()
+			stringGuess := strings.Fields(line)
+
+			if len(stringGuess) != 4 {
+				fmt.Println("Chute deve conter exatamente 4 números!")
+				continue
+			}
+
+			for position, stringGuessElement := range stringGuess {
+				intGuessElement, err := strconv.Atoi(stringGuessElement)
+
+				if err != nil || intGuessElement < 1 || intGuessElement > 8 {
+					fmt.Printf("Entrada '%s' inválida!\n", stringGuessElement)
+					continue OUTER
+				}
+
+				guess[position] = intGuessElement
+			}
+
+			break
+		}
+	}
+
+	return guess
 }
